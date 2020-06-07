092eb4868787c5c605c87f9e3b39318722fcd76e
rakesh0611 <66098832+rakesh0611@users.noreply.github.com>
2020-06-07 20:36:36 +0530

    added finding area of square using function

--- Square.py
+++ Square.py
@@ -3,3 +3,12 @@ side = int(input("Give side value:"))
 perimeter = 4*side
 
 print("Perimeter of Square is {:.2f}".format(perimeter))
+
+
+print("**********************************************")
+
+
+def area_of_square(side):
+    area = side ** 2
+    print("Area of square(using function) is {:.2f}".format(area))
+area_of_square(side)
